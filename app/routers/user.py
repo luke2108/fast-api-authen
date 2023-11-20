08259f7488af3b110a85bb49cc31c67bd7892e5a
luke2108 <luke210855@gmail.com>
2023-11-20 22:55:07 +0800

    add

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,11 +8,11 @@ router = APIRouter()
 
 
 @router.get('/me', response_model=schemas.UserResponse)
-def get_me(db: Session = Depends(get_db), user_id: str = Depends(oauth2.require_user)):
+async def get_me(db: Session = Depends(get_db), user_id: str = Depends(oauth2.require_user)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     return user
 
 @router.get('/{user_id}', response_model=schemas.UserResponse)
-def get_user(user_id: UUID, db: Session = Depends(get_db)):
+async def get_user(user_id: UUID, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     return user
